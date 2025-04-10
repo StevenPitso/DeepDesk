6d615846d3aaeaf50f8da0178fe6676f88cef715
StevenPitso <Onalennapitso771@gmail.com>
2025-04-10 20:58:45 +0200

    finished the basic logic  of ai_sorter.js about to test wish me luck .......

--- ai_sorter.js
+++ ai_sorter.js
@@ -9,13 +9,34 @@ const fs = require("fs");
 const path = require("path");
 
 
-const {spawnSync} = require() // maybe of sub_process
+const {spawnSync} = require("child_process") // maybe of sub_process
 
 
 function predictCategory(text){
    // use the predict.py here somehow somehow. 
+   const result = spawnSync("python", ["predict.py"], text,{
+     encoding: "utf-8"
+   });
+   return result.stdout.trim();
 }
 
 function sortFiles(folderPath){
    /// folders  creation  and placement of files .....
+
+   const fullPath = path.join(folderPath, file);
+
+   if(fs.lstatSync(fullPath).isFile()) continue;
+   if(!file.endsWith(".txt")) continue;
+
+
+   const content = fs.readFileSync(fullPath, utf-8);
+   const predicted = predictCategory(content);
+
+
+   const targetDir = path.join(folderPath, predicted);
+   if(fs.existsSync(targetDir)) fs.mkdirSync(targetDir);
+   
+   fs.renameSync(fullPath, path.join(targetDir, file));
+   
+
 }
