49f0c461dcb05f5b262d5964a7a4862ff4391a22
StevenPitso <Onalennapitso771@gmail.com>
2025-04-10 20:33:27 +0200

    Created model ("train_model.py") using sklearn  joblibs in the code base and started the model/data predictor

---
+++ train_model.py
@@ -0,0 +1,40 @@
+import os
+import joblib
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.pipeline import Pipeline
+import json
+
+
+def loadData(basePath):
+    text, labels = [] ,[]
+    for label in os.listdir(basePath):
+        folder = os.path.join(basePath,label)
+        for file in os.listdir(folder):
+            with open(os.path.join(folder,file), 'r', encoding='utf-8', errors='ignore' ) as f:
+                text.append(f.read())
+                labels.append(label)
+    return text, labels
+
+
+X, y = loadData("data")
+
+# Creating  our model now 
+
+model = Pipeline([('tfidf', TfidfVectorizer(stop_words='english'),
+                   ('clf', MultinomialNB())
+                )])
+
+model.fit(X, y)
+
+#For in case ../model doesn't exit 
+
+os.makedirs("model", exist_ok=True)
+joblib.dump(model, "model/text_model.pkl")
+
+#Save labels
+
+with open("model/labels.json", "w") as f:
+    json.dump(list(set(y)),f)
+
+print(" [ > ]Model trained and saved.")
