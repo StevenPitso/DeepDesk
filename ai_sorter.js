23277ac770779054c95c34e0fbed4ba86044d4d9
StevenPitso <Onalennapitso771@gmail.com>
2025-04-10 20:40:02 +0200

    start working on the ai_sorter.js file and plan  how file will work. :)

---
+++ ai_sorter.js
@@ -0,0 +1,9 @@
+
+
+/*
+*   This the brain or CNS of this project
+*   we need to  use the predictor "predict.py and use js to validate file formattes mostly extentsions create folders"
+*   for now I know I need a us filehandling operations
+*/
+const fs = require("fs");
+
---
+++ predict.py
@@ -0,0 +1,15 @@
+
+
+# predict.py  " filePath/txt file"
+
+import sys 
+import joblib
+
+model = joblib.load("model/text_model.pkl")
+
+
+text = sys.argv[1]
+modelPrediction = model.prediction([text])[0]
+# we need to see the output at some point
+print(modelPrediction)
+
